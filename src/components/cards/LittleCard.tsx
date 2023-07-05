22d893623263b4b00e4e346151d1fff2d342da5d
nayzflux <55506519+nayzflux@users.noreply.github.com>
2023-07-06 00:37:03 +0200

    Fix missing responsive for small card

--- src/components/cards/LittleCard.tsx
+++ src/components/cards/LittleCard.tsx
@@ -2,12 +2,12 @@ import OutlinedButton from "../buttons/OutlinedButton.tsx";
 
 function LittleCard({title, description, image}: {title: string; description: string; image: string}) {
     return (
-        <div className={`flex w-full text-gray`}>
-            <div className={"w-1/2 bg-green-forest py-6"}>
+        <div className={`flex flex-col md:flex-row w-full text-gray`}>
+            <div className={"md:w-1/2 bg-green-forest py-6"}>
                 <img src={image} alt={"Product Image"}/>
             </div>
 
-            <div className={"flex flex-col items-center justify-center px-20 space-y-6 text-center w-1/2 bg-green-light"}>
+            <div className={"flex flex-col items-center justify-center px-20 space-y-6 text-center md:w-1/2 bg-green-light py-4 md:py-0"}>
                 <h3 className={"font-semibold xl:text-2xl text-xl"}>
                     {title}
                 </h3>
